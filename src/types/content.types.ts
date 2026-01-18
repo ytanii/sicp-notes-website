64d636784eab309d15fb0f6ebd9d7ef79c3b0b21
ytanii <154643447+ytanii@users.noreply.github.com>
2026-01-17 19:57:12 -0500

    first commit

---
+++ src/types/content.types.ts
@@ -0,0 +1,22 @@
+import type { CollectionEntry } from 'astro:content'
+
+// Reading time interface
+export interface ReadingTime {
+  text: string
+  minutes: number
+  time: number
+  words: number
+}
+
+// TOC item interface
+export interface TOCItem {
+  level: number
+  text: string
+  id: string
+  index: number
+}
+
+// PostList component props interface
+export interface PostListProps {
+  posts: CollectionEntry<'posts'>[]
+}
